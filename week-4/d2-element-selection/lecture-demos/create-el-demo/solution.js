732f7b654a4bc509037d781dd43341d966491f04
SergeyGridin <gridin21@gmail.com>
2021-02-02 10:36:17 -0500

    modify w4d2

--- week-4/d2-element-selection/lecture-demos/create-el-demo/solution.js
+++ week-4/d2-element-selection/lecture-demos/create-el-demo/solution.js
@@ -1,82 +1,59 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  // * TODO = ADD H1 TO HEADER
 
-document.addEventListener('DOMContentLoaded', (event) => {
+  let header = document.getElementById("my-header");
 
-	
-	// * TODO = ADD H1 TO HEADER
-	
-	let header = document.getElementById('my-header');
-	
-	// method #1
-	// header.innerHTML = '<h1>Hello World</h1>';
+  // method #1
+  // header.innerHTML = "<h1>Hello World</h1>";
 
-	// this won't work
-	// header.innerText = '<h1>Hello World</h1>';
+  // this won't work
+  // header.innerText = "<h1>Hello World</h1>";
 
+  // method #2
+  // let h1 = document.createElement('h1');
+  // h1.innerText = 'Hello World';
+  // header.appendChild(h1)
 
-	// method #2
-	// let h1 = document.createElement('h1');
-	// h1.innerText = 'Hello World';
-	// header.appendChild(h1)
+  // method #3
+  // let h1 = document.createElement('h1');
+  // let text = document.createTextNode('Hello World');
+  // h1.appendChild(text);
+  // header.appendChild(h1);
 
+  // * TODO = ADD ITEMS TO COLOR AND NUMBER LISTS WITH CLASS NAME
 
-	// method #3
-	// let h1 = document.createElement('h1');
-	// let text = document.createTextNode('Hello World');
-	// h1.appendChild(text);
-	// header.appendChild(h1);
+  // example items = ['Red', 'Green', 'Blue']
+  // example parent = colorList
+  // example className = 'color__item';
 
+  // function addItems(items, parent, className) {
+  // 	items.forEach(item => {
+  // 		let li = document.createElement('li');
+  // 		li.innerText = item;
+  // 		li.setAttribute('class', className);
+  // 		parent.appendChild(li);
+  // 	})
+  // }
 
+  // let colors = ['Red', 'Green', 'Blue']
+  // let colorUl = document.getElementById('colors')
+  // let colorClass = "color__item"
 
+  // addItems(colors, colorUl, colorClass);
 
-// * TODO = ADD ITEMS TO COLOR AND NUMBER LISTS WITH CLASS NAME
+  // let nums = ['One', 'Two', 'Three'];
+  // let numUl = document.getElementById('numbers');
+  // let numClass = 'number__item';
 
-// example items = ['Red', 'Green', 'Blue']
-// example parent = colorList
-// example className = 'color__item';
-
-	function addItems(items, parent, className) {
-		items.forEach(item => {
-			let li = document.createElement('li');
-			li.innerText = item;
-			li.setAttribute('class', className);
-			parent.appendChild(li);
-		})
-	}
-
-	let colors = ['Red', 'Green', 'Blue']
-	let colorUl = document.getElementById('colors')
-	let colorClass = "color__item"
-
-	addItems(colors, colorUl, colorClass);
-
-	let nums = ['One', 'Two', 'Three'];
-	let numUl = document.getElementById('numbers');
-	let numClass = 'number__item';
-
-	addItems(nums, numUl, numClass)
-
-	
+  // addItems(nums, numUl, numClass)
 });
 
-
-
-
-
-
-
-
-
-
-
 // // SEPARATE EXAMPLE BELOW
-
 
 // 	function addItem(parentName) {
 // 		let body = document.querySelector(parentName);
 // 		body.innerHTML = '<div>hello</div>';
 // 	}
-
-
 
 // window.onload = addItem;
 
@@ -85,6 +62,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
 // }
 
 // document.addEventListener('DOMContentLoaded', callback);
-
-
-
